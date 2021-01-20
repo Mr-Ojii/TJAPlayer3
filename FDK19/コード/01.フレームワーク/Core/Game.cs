86b2e89bd1dc34fa95b01f9a1e4c6773ff725b0d
Mr-Ojii <okaschan@gmail.com>
2021-01-21 02:57:29 +0900

    dllのフォルダ名にOSプラットフォーム名の追加

--- FDK19/コード/01.フレームワーク/Core/Game.cs
+++ FDK19/コード/01.フレームワーク/Core/Game.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Reflection;
 using System.IO;
 using System.Text;
+using System.Runtime.InteropServices;
 using OpenTK;
 using OpenTK.Graphics;
 
@@ -29,14 +30,23 @@ namespace FDK
 		public Game()
 			: base(GameWindowSize.Width, GameWindowSize.Height, GraphicsMode.Default, "TJAP3-f(OpenGL)Alpha")
 		{
-			string platform = Environment.Is64BitProcess ? "x64" : "x86";			
 
-			FFmpeg.AutoGen.ffmpeg.RootPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"/ffmpeg/" + platform + "/";
+			string osplatform = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "win" : (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? "osx" : "linux");
+			string platform = Environment.Is64BitProcess ? "x64" : "x86";
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) 
+			{
+				if(Environment.Is64BitProcess)
+					FFmpeg.AutoGen.ffmpeg.RootPath = "/usr/lib/x86_64-linux-gnu/";
+				else
+					FFmpeg.AutoGen.ffmpeg.RootPath = "/usr/lib/i386-linux-gnu/";
+			}
+			else 
+				FFmpeg.AutoGen.ffmpeg.RootPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"/ffmpeg/" + osplatform + "-" + platform + "/";
 
 			if (!Directory.Exists(FFmpeg.AutoGen.ffmpeg.RootPath))
 				throw new DirectoryNotFoundException("FFmpeg RootPath Not Found.\nPath=" + FFmpeg.AutoGen.ffmpeg.RootPath);
 
-			DirectoryInfo info = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"/dll/" + platform + "/");
+			DirectoryInfo info = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"/dll/" + osplatform + "-" + platform + "/");
 
 			//exeの階層にdllをコピー
 			foreach (FileInfo fileinfo in info.GetFiles())
