06ffec6241bd092fddbb8a198a1b7fac13278477
Mr-Ojii <okaschan@gmail.com>
2021-08-27 16:19:43 +0900

    正常に動作していないコードの削除

--- TJAPlayer3-f/src/Common/TJAPlayer3.cs
+++ TJAPlayer3-f/src/Common/TJAPlayer3.cs
@@ -302,8 +302,6 @@ namespace TJAPlayer3
 				{
 					base.ClientSize =
 						new Size(currentClientSize.Width, currentClientSize.Height);
-					base.Icon = new Icon(Assembly.GetExecutingAssembly().GetManifestResourceStream("TJAPlayer3.TJAPlayer3-f.ico"));
-					//					FDK.CTaskBar.ShowTaskBar( true );
 				}
 			}
 		}
@@ -312,18 +310,6 @@ namespace TJAPlayer3
 
 		protected override void OnLoad(EventArgs e)
 		{
-			if (ConfigIni.bウィンドウモード)
-			{
-				if (!this.CursorVisible)
-				{
-					this.CursorVisible = true;
-				}
-			}
-			else if (this.CursorVisible)
-			{
-				this.CursorVisible = false;
-			}
-
 			CAction.LoadContentAction();
 
 			if (this.listトップレベルActivities != null)
@@ -367,32 +353,6 @@ namespace TJAPlayer3
 		}
 		protected override void OnRenderFrame(FrameEventArgs e)
 		{
-			if (this.D != Direction.None)
-			{
-				Console.WriteLine(D);
-				Rectangle rect = new Rectangle(base.X, base.Y, ClientSize.Width, ClientSize.Height);
-				if ((D & Direction.Right) == Direction.Right || (D & Direction.Left) == Direction.Left)
-				{
-					rect = new Rectangle(base.X, base.Y, ClientSize.Width, (int)(((double)GameWindowSize.Height / GameWindowSize.Width) * ClientSize.Width));
-				}
-				else if ((D & Direction.Bottom) == Direction.Bottom || (D & Direction.Top) == Direction.Top)
-				{
-					rect = new Rectangle(base.X, base.Y, (int)(((double)GameWindowSize.Width / GameWindowSize.Height) * ClientSize.Height), ClientSize.Height);
-				}
-
-				if (ConfigIni.bウィンドウモード)
-				{
-					ConfigIni.n初期ウィンドウ開始位置X = rect.X;   // #30675 2013.02.04 ikanick add
-					ConfigIni.n初期ウィンドウ開始位置Y = rect.Y;   //
-				}
-
-				ConfigIni.nウインドウwidth = (ConfigIni.bウィンドウモード) ? rect.Width : currentClientSize.Width;    // #23510 2010.10.31 yyagi add
-				ConfigIni.nウインドウheight = (ConfigIni.bウィンドウモード) ? rect.Height : currentClientSize.Height;
-
-				this.ClientRectangle = rect;
-				D = Direction.None;
-				return;
-			}
 			Timer?.t更新();
 			CSoundManager.rc演奏用タイマ?.t更新();
 			InputManager?.tSwapEventList();
@@ -1932,35 +1892,15 @@ namespace TJAPlayer3
 
 		private void Window_ResizeOrMove(object sender, EventArgs e)               // #23510 2010.11.20 yyagi: to get resized window size
 		{
-			if (ConfigIni.n初期ウィンドウ開始位置X != base.X)
+			if (ConfigIni.bウィンドウモード)
 			{
-				D |= Direction.Left;
-			}
-			else if (ConfigIni.nウインドウwidth != base.ClientSize.Width)
-			{
-				D |= Direction.Right;
+				ConfigIni.n初期ウィンドウ開始位置X = this.X;   // #30675 2013.02.04 ikanick add
+				ConfigIni.n初期ウィンドウ開始位置Y = this.Y;   //
 			}
 
-			if (ConfigIni.n初期ウィンドウ開始位置Y != base.Y)
-			{
-				D |= Direction.Top;
-			}
-			else if (ConfigIni.nウインドウheight != base.ClientSize.Height)
-			{
-				D |= Direction.Bottom;
-			}
+			ConfigIni.nウインドウwidth = (ConfigIni.bウィンドウモード) ? this.Width : currentClientSize.Width;    // #23510 2010.10.31 yyagi add
+			ConfigIni.nウインドウheight = (ConfigIni.bウィンドウモード) ? this.Height : currentClientSize.Height;
 		}
-
-		[Flags]
-		private enum Direction : int 
-		{
-			None = 0,
-			Top,
-			Bottom,
-			Left,
-			Right
-		}
-		private Direction D = Direction.None;
 
 #endregion
 #endregion
