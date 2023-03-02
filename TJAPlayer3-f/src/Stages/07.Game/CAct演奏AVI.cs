31f6767fd0eb6a255f4700f7239a1409b630e036
Mr-Ojii <okaschan@gmail.com>
2023-03-02 21:00:00 +0900

    CAct演奏AVI: 整理

--- TJAPlayer3-f/src/Stages/07.Game/CAct演奏AVI.cs
+++ TJAPlayer3-f/src/Stages/07.Game/CAct演奏AVI.cs
@@ -24,28 +24,18 @@ namespace TJAPlayer3
 			{
 				this.rVD = rVD;
 				if (this.rVD != null)
-				{					
+				{
 					this.ratio1 = Math.Min((float)GameWindowSize.Height / ((float)this.rVD.FrameSize.Height), (float)GameWindowSize.Width / ((float)this.rVD.FrameSize.Height));
 					
 					this.rVD.Start();
 				}
 			}
 		}
-		public void Seek( int n再生開始時刻ms )
-		{
-			if (this.rVD != null)
-			{
-				this.rVD.Seek(n再生開始時刻ms);
-			}
-		}
+		public void Seek( int ms ) => this.rVD?.Seek(ms);
 
-		public void Stop()
-		{
-			if (this.rVD != null)
-			{
-				this.rVD.Stop();
-			}
-		}
+		public void Stop() => this.rVD?.Stop();
+		
+		public void tPauseControl() => this.rVD?.PauseControl();
 
 		public unsafe int t進行描画()
 		{
@@ -53,53 +43,32 @@ namespace TJAPlayer3
 			{
 				if (this.rVD == null)
 					return 0;
+					
+				this.rVD.GetNowFrame(ref this.tx描画用);
 
-				if (this.rVD != null)
+				this.tx描画用.vcScaling.X = this.ratio1;
+				this.tx描画用.vcScaling.Y = this.ratio1;
+
+				if (TJAPlayer3.ConfigIni.eClipDispType.HasFlag(EClipDispType.Background))
 				{
-					#region[ ワイドクリップ ]
-					this.rVD.GetNowFrame(ref this.tx描画用);
-
-					this.tx描画用.vcScaling.X = this.ratio1;
-					this.tx描画用.vcScaling.Y = this.ratio1;
-
-					if (TJAPlayer3.ConfigIni.eClipDispType.HasFlag(EClipDispType.Background))
-					{
-						this.tx描画用.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, GameWindowSize.Width / 2, GameWindowSize.Height / 2);
-					}
-					#endregion
+					this.tx描画用.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, GameWindowSize.Width / 2, GameWindowSize.Height / 2);
 				}
-				
 			}
 			return 0;
 		}
 
 		public void t窓表示()
 		{
-			if( this.rVD != null )
-			{
-				#region[ ワイドクリップ ]
+			if( this.rVD == null || this.tx描画用 == null || !TJAPlayer3.ConfigIni.eClipDispType.HasFlag(EClipDispType.Window))
+				return;
+				
+			float[] fRatio = new float[] { 640.0f - 4.0f, 360.0f - 4.0f }; //中央下表示
 
-				if( this.tx描画用 != null )
-				{
-					float[] fRatio = new float[] { 640.0f - 4.0f, 360.0f - 4.0f }; //中央下表示
+			float ratio = Math.Min((float)(fRatio[0] / this.rVD.FrameSize.Width), (float)(fRatio[1] / this.rVD.FrameSize.Height));
+			this.tx描画用.vcScaling.X = ratio;
+			this.tx描画用.vcScaling.Y = ratio;
 
-					float ratio = Math.Min((float)(fRatio[0] / this.rVD.FrameSize.Width), (float)(fRatio[1] / this.rVD.FrameSize.Height));
-					this.tx描画用.vcScaling.X = ratio;
-					this.tx描画用.vcScaling.Y = ratio;
-
-					this.tx描画用.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Down, GameWindowSize.Width / 2, GameWindowSize.Height);
-				}
-
-				#endregion
-			}
-		}
-
-		public void tPauseControl()
-		{
-			if (this.rVD != null)
-			{
-				this.rVD.PauseControl();
-			}
+			this.tx描画用.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Down, GameWindowSize.Width / 2, GameWindowSize.Height);
 		}
 
 		// CActivity 実装
