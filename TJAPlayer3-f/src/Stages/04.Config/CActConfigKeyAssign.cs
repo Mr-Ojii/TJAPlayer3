b139c01a802cd77590911bb2201e8636fbe3fe56
Mr-Ojii <okaschan@gmail.com>
2023-05-04 18:21:31 +0900

    KeyLabelのDictionary化

--- TJAPlayer3-f/src/Stages/04.Config/CActConfigKeyAssign.cs
+++ TJAPlayer3-f/src/Stages/04.Config/CActConfigKeyAssign.cs
@@ -198,30 +198,26 @@ internal class CActConfigKeyAssign : CActivity
 	// その他
 
 	#region [ private ]
-	//-----------------
-	[StructLayout( LayoutKind.Sequential )]
-	private struct STKEYLABEL
-	{
-		public int nCode;
-		public string strLabel;
-		public STKEYLABEL( int nCode, string strLabel )
-		{
-			this.nCode = nCode;
-			this.strLabel = strLabel;
-		}
-	}
-
 	private bool bキー入力待ち;
-	private STKEYLABEL[] KeyLabel = new STKEYLABEL[] { 
-		new STKEYLABEL(0x35, "[ESC]"), new STKEYLABEL(1, "[ 1 ]"), new STKEYLABEL(2, "[ 2 ]"), new STKEYLABEL(3, "[ 3 ]"), new STKEYLABEL(4, "[ 4 ]"), new STKEYLABEL(5, "[ 5 ]"), new STKEYLABEL(6, "[ 6 ]"), new STKEYLABEL(7, "[ 7 ]"), new STKEYLABEL(8, "[ 8 ]"), new STKEYLABEL(9, "[ 9 ]"), new STKEYLABEL(0, "[ 0 ]"), new STKEYLABEL(0x53, "[ - ]"), new STKEYLABEL(0x34, "[ = ]"), new STKEYLABEL(0x2a, "[BSC]"), new STKEYLABEL(0x81, "[TAB]"), new STKEYLABEL(0x1a, "[ Q ]"), 
-		new STKEYLABEL(0x20, "[ W ]"), new STKEYLABEL(14, "[ E ]"), new STKEYLABEL(0x1b, "[ R ]"), new STKEYLABEL(0x1d, "[ T ]"), new STKEYLABEL(0x22, "[ Y ]"), new STKEYLABEL(30, "[ U ]"), new STKEYLABEL(0x12, "[ I ]"), new STKEYLABEL(0x18, "[ O ]"), new STKEYLABEL(0x19, "[ P ]"), new STKEYLABEL(0x4a, "[ [ ]"), new STKEYLABEL(0x73, "[ ] ]"), new STKEYLABEL(0x75, "[Enter]"), new STKEYLABEL(0x4b, "[L-Ctrl]"), new STKEYLABEL(10, "[ A ]"), new STKEYLABEL(0x1c, "[ S ]"), new STKEYLABEL(13, "[ D ]"), 
-		new STKEYLABEL(15, "[ F ]"), new STKEYLABEL(0x10, "[ G ]"), new STKEYLABEL(0x11, "[ H ]"), new STKEYLABEL(0x13, "[ J ]"), new STKEYLABEL(20, "[ K ]"), new STKEYLABEL(0x15, "[ L ]"), new STKEYLABEL(0x7b, "[ ; ]"), new STKEYLABEL(0x26, "[ ' ]"), new STKEYLABEL(0x45, "[ ` ]"), new STKEYLABEL(0x4e, "[L-Shift]"), new STKEYLABEL(0x2b, @"[ \]"), new STKEYLABEL(0x23, "[ Z ]"), new STKEYLABEL(0x21, "[ X ]"), new STKEYLABEL(12, "[ C ]"), new STKEYLABEL(0x1f, "[ V ]"), new STKEYLABEL(11, "[ B ]"), 
-		new STKEYLABEL(0x17, "[ N ]"), new STKEYLABEL(0x16, "[ M ]"), new STKEYLABEL(0x2f, "[ , ]"), new STKEYLABEL(0x6f, "[ . ]"), new STKEYLABEL(0x7c, "[ / ]"), new STKEYLABEL(120, "[R-Shift]"), new STKEYLABEL(0x6a, "[ * ]"), new STKEYLABEL(0x4d, "[L-Alt]"), new STKEYLABEL(0x7e, "[Space]"), new STKEYLABEL(0x2d, "[CAPS]"), new STKEYLABEL(0x36, "[F1]"), new STKEYLABEL(0x37, "[F2]"), new STKEYLABEL(0x38, "[F3]"), new STKEYLABEL(0x39, "[F4]"), new STKEYLABEL(0x3a, "[F5]"), new STKEYLABEL(0x3b, "[F6]"), 
-		new STKEYLABEL(60, "[F7]"), new STKEYLABEL(0x3d, "[F8]"), new STKEYLABEL(0x3e, "[F9]"), new STKEYLABEL(0x3f, "[F10]"), new STKEYLABEL(0x58, "[NumLock]"), new STKEYLABEL(0x7a, "[Scroll]"), new STKEYLABEL(0x60, "[NPad7]"), new STKEYLABEL(0x61, "[NPad8]"), new STKEYLABEL(0x62, "[NPad9]"), new STKEYLABEL(0x66, "[NPad-]"), new STKEYLABEL(0x5d, "[NPad4]"), new STKEYLABEL(0x5e, "[NPad5]"), new STKEYLABEL(0x5f, "[NPad6]"), new STKEYLABEL(0x68, "[NPad+]"), new STKEYLABEL(90, "[NPad1]"), new STKEYLABEL(0x5b, "[NPad2]"), 
-		new STKEYLABEL(0x5c, "[NPad3]"), new STKEYLABEL(0x59, "[NPad0]"), new STKEYLABEL(0x67, "[NPad.]"), new STKEYLABEL(0x40, "[F11]"), new STKEYLABEL(0x41, "[F12]"), new STKEYLABEL(0x42, "[F13]"), new STKEYLABEL(0x43, "[F14]"), new STKEYLABEL(0x44, "[F15]"), new STKEYLABEL(0x48, "[Kana]"), new STKEYLABEL(0x24, "[ ? ]"), new STKEYLABEL(0x30, "[Henkan]"), new STKEYLABEL(0x57, "[MuHenkan]"), new STKEYLABEL(0x8f, @"[ \ ]"), new STKEYLABEL(0x25, "[NPad.]"), new STKEYLABEL(0x65, "[NPad=]"), new STKEYLABEL(0x72, "[ ^ ]"), 
-		new STKEYLABEL(40, "[ @ ]"), new STKEYLABEL(0x2e, "[ : ]"), new STKEYLABEL(130, "[ _ ]"), new STKEYLABEL(0x49, "[Kanji]"), new STKEYLABEL(0x7f, "[Stop]"), new STKEYLABEL(0x29, "[AX]"), new STKEYLABEL(100, "[NPEnter]"), new STKEYLABEL(0x74, "[R-Ctrl]"), new STKEYLABEL(0x54, "[Mute]"), new STKEYLABEL(0x2c, "[Calc]"), new STKEYLABEL(0x70, "[PlayPause]"), new STKEYLABEL(0x52, "[MediaStop]"), new STKEYLABEL(0x85, "[Volume-]"), new STKEYLABEL(0x86, "[Volume+]"), new STKEYLABEL(0x8b, "[WebHome]"), new STKEYLABEL(0x63, "[NPad,]"), 
-		new STKEYLABEL(0x69, "[ / ]"), new STKEYLABEL(0x80, "[PrtScn]"), new STKEYLABEL(0x77, "[R-Alt]"), new STKEYLABEL(110, "[Pause]"), new STKEYLABEL(70, "[Home]"), new STKEYLABEL(0x84, "[Up]"), new STKEYLABEL(0x6d, "[PageUp]"), new STKEYLABEL(0x4c, "[Left]"), new STKEYLABEL(0x76, "[Right]"), new STKEYLABEL(0x33, "[End]"), new STKEYLABEL(50, "[Down]"), new STKEYLABEL(0x6c, "[PageDown]"), new STKEYLABEL(0x47, "[Insert]"), new STKEYLABEL(0x31, "[Delete]"), new STKEYLABEL(0x4f, "[L-Win]"), new STKEYLABEL(0x79, "[R-Win]"), 
-		new STKEYLABEL(0x27, "[APP]"), new STKEYLABEL(0x71, "[Power]"), new STKEYLABEL(0x7d, "[Sleep]"), new STKEYLABEL(0x87, "[Wake]")
+	private Dictionary<int, string> KeyLabel = new() {
+		{ 0x00, "[ 0 ]" }, { 0x01, "[ 1 ]" }, { 0x02, "[ 2 ]" }, { 0x03, "[ 3 ]" }, { 0x04, "[ 4 ]" }, { 0x05, "[ 5 ]" }, { 0x06, "[ 6 ]" }, { 0x07, "[ 7 ]" }, { 0x08, "[ 8 ]" }, { 0x09, "[ 9 ]" },
+		{ 0x0a, "[ A ]" }, { 0x0b, "[ B ]" }, { 0x0c, "[ C ]" }, { 0x0d, "[ D ]" }, { 0x0e, "[ E ]" }, { 0x0f, "[ F ]" }, { 0x10, "[ G ]" }, { 0x11, "[ H ]" }, { 0x12, "[ R ]" }, { 0x13, "[ J ]" },
+		{ 0x14, "[ K ]" }, { 0x15, "[ L ]" }, { 0x16, "[ M ]" }, { 0x17, "[ N ]" }, { 0x18, "[ O ]" }, { 0x19, "[ P ]" }, { 0x1a, "[ Q ]" }, { 0x1b, "[ R ]" }, { 0x1c, "[ S ]" }, { 0x1d, "[ T ]" },
+		{ 0x1e, "[ U ]" }, { 0x1f, "[ V ]" }, { 0x20, "[ W ]" }, { 0x21, "[ X ]" }, { 0x22, "[ Y ]" }, { 0x23, "[ Z ]" },
+		{ 0x24, "[ ? ]" }, { 0x25, "[NPad.]" }, { 0x26, "[ ' ]" }, { 0x27, "[ APP ]" }, { 0x28, "[ @ ]" }, { 0x29, "[AX]" }, { 0x2a, "[BSC]" }, { 0x2b, @"[ \ ]" }, { 0x2c, "[Calc]" }, { 0x2d, "[CAPS]" },
+		{ 0x2e, "[ : ]" }, { 0x2f, "[ , ]" }, { 0x30, "[Henkan]" }, { 0x31, "[Delete]" }, { 0x32, "[Down]" }, { 0x33, "[End]" }, { 0x34, "[ = ]" }, { 0x35, "[ESC]" },
+		{ 0x36, "[F1]" }, { 0x37, "[F2]" }, { 0x38, "[F3]" }, { 0x39, "[F4]" }, { 0x3a, "[F5]" }, { 0x3b, "[F6]" }, { 0x3c, "[F7]" }, { 0x3d, "[F8]" },
+		{ 0x3e, "[F9]" }, { 0x3f, "[F10]" }, { 0x40, "[F11]" }, { 0x41, "[F12]" }, { 0x42, "[F13]" }, { 0x43, "[F14]" }, { 0x44, "[F15]" },
+		{ 0x45, "[ ` ]" }, { 0x46, "[Home]" }, { 0x47, "[Insert]" }, { 0x48, "[Kana]" }, { 0x49, "[Kanji]" }, { 0x4a, "[ [ ]" },
+		{ 0x4b, "[L-Ctrl]" }, { 0x4c, "[Left]" }, { 0x4d, "[L-Alt]" }, { 0x4e, "[L-Shift]" }, { 0x4f, "[L-Win]" },
+		{ 0x52, "[MediaStop]" }, { 0x53, "[ - ]" }, { 0x54, "[Mute]" }, { 0x57, "[MuHenkan]" }, { 0x58, "[NumLock]" },
+		{ 0x59, "[NPad0]" }, { 0x5a, "[NPad1]" }, { 0x5b, "[NPad2]" }, { 0x5c, "[NPad3]" }, { 0x5d, "[NPad4]" }, { 0x5e, "[NPad5]" }, { 0x5f, "[NPad6]" }, { 0x60, "[NPad7]" }, { 0x61, "[NPad8]" }, { 0x62, "[NPad9]" },
+		{ 0x63, "[NPad,]" }, { 0x64, "[NPEnter]" }, { 0x65, "[NPad=]" }, { 0x66, "[NPad-]" }, { 0x67, "[NPad.]" }, { 0x68, "[NPad+]" },
+		{ 0x69, "[ / ]" }, { 0x6a, "[ * ]" }, { 0x6c, "[PgDn]" }, { 0x6d, "[PgUp]" }, { 0x6e, "[Pause]" }, { 0x6f, "[ . ]" },
+		{ 0x70, "[PlayPause]" }, { 0x71, "[Power]" }, { 0x72, "[ ^ ]" }, { 0x73, "[ ] ]" },
+		{ 0x74, "[R-Ctrl]" }, { 0x75, "[Enter]" }, { 0x76, "[Right]" }, { 0x77, "[R-Alt]" }, { 0x78, "[R-Shift]" }, { 0x79, "[R-Win]" },
+		{ 0x7a, "[Scroll]" }, { 0x7b, "[ ; ]" }, { 0x7c, "[ / ]" }, { 0x7d, "[Sleep]" }, { 0x7e, "[Space]" }, { 0x7f, "[Stop]" }, { 0x80, "[PrtScn]" },
+		{ 0x81, "[TAB]" }, { 0x82, "[ _ ]" }, { 0x84, "[Up]" }, { 0x85, "[Volume-]" }, { 0x86, "[Volume+]" }, { 0x87, "[Wake]" }, { 0x8b, "[WebHome]" }, { 0x8f, "[ / ]" },
 	};
 	private int n現在の選択行;
 	private EKeyConfigPad pad;
@@ -286,15 +282,11 @@ internal class CActConfigKeyAssign : CActivity
 	private void tアサインコードの描画_Keyboard( int line, int x, int y, int nID, int nCode, bool b強調 )
 	{
 		string str = null;
-		foreach( STKEYLABEL stkeylabel in this.KeyLabel )
+		if(this.KeyLabel.TryGetValue(nCode, out var strLabel))
 		{
-			if( stkeylabel.nCode == nCode )
-			{
-				str = string.Format( "{0,2}. Key {1}", line, stkeylabel.strLabel );
-				break;
-			}
+			str = string.Format( "{0,2}. Key {1}", line, strLabel );
 		}
-		if( str == null )
+		else
 		{
 			str = string.Format( "{0,2}. Key 0x{1:X2}", line, nCode );
 		}
