12b480152dc291bd35fa987366ef12bc7dd100e1
Mr-Ojii <okaschan@gmail.com>
2023-08-14 22:21:26 +0900

    BoxDefのパース方法を変更

--- TJAPlayer3-f/src/Songs/CBoxDef.cs
+++ TJAPlayer3-f/src/Songs/CBoxDef.cs
@@ -21,54 +21,47 @@ internal class CBoxDef
 
     public CBoxDef( string boxdefFileName )
     {
-        Encoding boxdefEnc = CJudgeTextEncoding.JudgeFileEncoding(boxdefFileName);
-        using (StreamReader reader = new StreamReader(boxdefFileName, boxdefEnc))
+        string[] strs = CJudgeTextEncoding.ReadTextFile(boxdefFileName).Split("\n", StringSplitOptions.RemoveEmptyEntries);
+        foreach(var stri in strs)
         {
-            string str = null;
-            while ((str = reader.ReadLine()) != null)
+            var str = stri.TrimStart(' ', '\t');
+
+            if (str.IndexOf(';') != -1)
+                str = str.Substring(0, str.IndexOf(';'));
+
+            if (str.Length == 0)
+                continue;
+
+            if (str[0] != '#')
+                continue;
+
+            try
             {
-                if (str.Length == 0)
-                    continue;
-                    
-                try
+                char[] ignoreChars = new char[] { ':', ' ', '\t' };
+
+                if (str.StartsWith("#TITLE", StringComparison.OrdinalIgnoreCase))
                 {
-                    char[] ignoreCharsWoColon = new char[] { ' ', '\t' };
-
-                    str = str.TrimStart(ignoreCharsWoColon);
-                    if ((str[0] == '#') && (str[0] != ';'))
-                    {
-                        if (str.IndexOf(';') != -1)
-                        {
-                            str = str.Substring(0, str.IndexOf(';'));
-                        }
-
-                        char[] ignoreChars = new char[] { ':', ' ', '\t' };
-
-                        if (str.StartsWith("#TITLE", StringComparison.OrdinalIgnoreCase))
-                        {
-                            this.Title = str.Substring(6).Trim(ignoreChars);
-                        }
-                        else if (str.StartsWith("#GENRE", StringComparison.OrdinalIgnoreCase))
-                        {
-                            this.Genre = str.Substring(6).Trim(ignoreChars);
-                        }
-                        else if (str.StartsWith("#FORECOLOR", StringComparison.OrdinalIgnoreCase))
-                        {
-                            this.ForeColor = ColorTranslator.FromHtml(str.Substring(10).Trim(ignoreChars));
-                        }
-                        else if (str.StartsWith("#BACKCOLOR", StringComparison.OrdinalIgnoreCase))
-                        {
-                            this.BackColor = ColorTranslator.FromHtml(str.Substring(10).Trim(ignoreChars));
-                        }
-                    }
-                    continue;
+                    this.Title = str.Substring(6).Trim(ignoreChars);
                 }
-                catch (Exception e)
+                else if (str.StartsWith("#GENRE", StringComparison.OrdinalIgnoreCase))
                 {
-                    Trace.TraceError(e.ToString());
-                    Trace.TraceError("An exception has occurred, but processing continues.");
-                    continue;
+                    this.Genre = str.Substring(6).Trim(ignoreChars);
                 }
+                else if (str.StartsWith("#FORECOLOR", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.ForeColor = ColorTranslator.FromHtml(str.Substring(10).Trim(ignoreChars));
+                }
+                else if (str.StartsWith("#BACKCOLOR", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.BackColor = ColorTranslator.FromHtml(str.Substring(10).Trim(ignoreChars));
+                }
+                continue;
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError(e.ToString());
+                Trace.TraceError("An exception has occurred, but processing continues.");
+                continue;
             }
         }
     }
