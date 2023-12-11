f9e289f67a691f130353972887a2aa9a19921cec
Mr-Ojii <okaschan@gmail.com>
2023-12-11 12:39:07 +0900

    BassMixExtensions ラムダ式に

--- FDK19/src/03.Sound/ExtensionMethods/BassMixExtensions.cs
+++ FDK19/src/03.Sound/ExtensionMethods/BassMixExtensions.cs
@@ -9,17 +9,11 @@ namespace FDK.BassMixExtension;
 public static class BassMixExtensions
 {
     public static bool ChannelPlay(int hHandle)
-    {
-        return BassMix.ChannelRemoveFlag(hHandle, BassFlags.MixerChanPause);
-    }
+        => BassMix.ChannelRemoveFlag(hHandle, BassFlags.MixerChanPause);
 
     public static bool ChannelPause(int hHandle)
-    {
-        return BassMix.ChannelAddFlag(hHandle, BassFlags.MixerChanPause);
-    }
+        => BassMix.ChannelAddFlag(hHandle, BassFlags.MixerChanPause);
 
     public static bool ChannelIsPlaying(int hHandle)
-    {
-        return !BassMix.ChannelHasFlag(hHandle, BassFlags.MixerChanPause);
-    }
+        => !BassMix.ChannelHasFlag(hHandle, BassFlags.MixerChanPause);
 }
