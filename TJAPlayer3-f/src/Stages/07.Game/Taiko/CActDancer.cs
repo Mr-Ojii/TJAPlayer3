ee68494d8d667d4a0874caf536477bb36c281635
Mr-Ojii <okaschan@gmail.com>
2024-03-03 06:34:17 +0900

    CActDancer: nullable

--- TJAPlayer3-f/src/Stages/07.Game/Taiko/CActDancer.cs
+++ TJAPlayer3-f/src/Stages/07.Game/Taiko/CActDancer.cs
@@ -32,16 +32,19 @@ internal class CActDancer : CActivity
             this.b初めての進行描画 = false;
         }
 
-        if (this.ct踊り子モーション != null || TJAPlayer3.app.Skin.Game_Dancer_Ptn != 0) this.ct踊り子モーション.t進行LoopDb();
-
-        if (TJAPlayer3.app.ConfigToml.Game.ShowDancer && this.ct踊り子モーション != null && TJAPlayer3.app.Skin.Game_Dancer_Ptn != 0)
+        if (this.ct踊り子モーション != null && TJAPlayer3.app.Skin.Game_Dancer_Ptn != 0)
         {
-            for (int i = 0; i < TJAPlayer3.Tx.Dancer.Length; i++)
+            this.ct踊り子モーション.t進行LoopDb();
+
+            if (TJAPlayer3.app.ConfigToml.Game.ShowDancer)
             {
-                if (TJAPlayer3.Tx.Dancer[i][this.ar踊り子モーション番号[(int)this.ct踊り子モーション.db現在の値]] != null)
+                for (int i = 0; i < TJAPlayer3.Tx.Dancer.Length; i++)
                 {
-                    if ((int)TJAPlayer3.stage演奏ドラム画面.actGauge.db現在のゲージ値[0] >= TJAPlayer3.app.Skin.SkinConfig.Game.Dancer.Gauge[i])
-                        TJAPlayer3.Tx.Dancer[i][this.ar踊り子モーション番号[(int)this.ct踊り子モーション.db現在の値]].t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Dancer.X[i], TJAPlayer3.app.Skin.SkinConfig.Game.Dancer.Y[i]);
+                    if (TJAPlayer3.Tx.Dancer[i][this.ar踊り子モーション番号[(int)this.ct踊り子モーション.db現在の値]] != null)
+                    {
+                        if ((int)TJAPlayer3.stage演奏ドラム画面.actGauge.db現在のゲージ値[0] >= TJAPlayer3.app.Skin.SkinConfig.Game.Dancer.Gauge[i])
+                            TJAPlayer3.Tx.Dancer[i][this.ar踊り子モーション番号[(int)this.ct踊り子モーション.db現在の値]].t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Dancer.X[i], TJAPlayer3.app.Skin.SkinConfig.Game.Dancer.Y[i]);
+                    }
                 }
             }
         }
@@ -50,8 +53,8 @@ internal class CActDancer : CActivity
 
     #region[ private ]
     //-----------------
-    public int[] ar踊り子モーション番号;
-    public CCounter ct踊り子モーション;
+    public int[] ar踊り子モーション番号 = new int[] { 0, 0 };
+    public CCounter? ct踊り子モーション;
     //-----------------
     #endregion
 }
