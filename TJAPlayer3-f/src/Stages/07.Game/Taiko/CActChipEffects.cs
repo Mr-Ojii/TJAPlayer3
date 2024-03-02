2199530dde39f75e4fb03f60dfbb0e8ad7b7fcb1
Mr-Ojii <okaschan@gmail.com>
2024-03-03 06:36:45 +0900

    CActChipEffects: nullable

--- TJAPlayer3-f/src/Stages/07.Game/Taiko/CActChipEffects.cs
+++ TJAPlayer3-f/src/Stages/07.Game/Taiko/CActChipEffects.cs
@@ -56,33 +56,34 @@ internal class CActChipEffects : CActivity
     {
         for (int i = 0; i < 128; i++)
         {
-            if (!st[i].b使用中)
+            var si = st[i];
+            if (!si.b使用中 || si.ct進行 == null)
                 continue;
 
-            st[i].ct進行.t進行();
-            if (st[i].ct進行.b終了値に達した)
+            si.ct進行.t進行();
+            if (si.ct進行.b終了値に達した)
             {
-                st[i].ct進行.t停止();
-                st[i].b使用中 = false;
+                si.ct進行.t停止();
+                si.b使用中 = false;
             }
-            TJAPlayer3.Tx.Notes.Opacity = 255 - (int)Math.Min((500.0 * (st[i].ct進行.n現在の値 / (double)st[i].ct進行.n終了値)), 255.0);
+            TJAPlayer3.Tx.Notes.Opacity = 255 - (int)Math.Min((500.0 * (si.ct進行.n現在の値 / (double)si.ct進行.n終了値)), 255.0);
             if (TJAPlayer3.Tx.Notes_White != null)
-                TJAPlayer3.Tx.Notes_White.Opacity = (int)Math.Min((500.0 * (st[i].ct進行.n現在の値 / (double)st[i].ct進行.n終了値)), 255.0);//2020.05.15 Mr-Ojii ノーツを白くするために追加。
-            switch (st[i].nプレイヤー)
+                TJAPlayer3.Tx.Notes_White.Opacity = (int)Math.Min((500.0 * (si.ct進行.n現在の値 / (double)si.ct進行.n終了値)), 255.0);//2020.05.15 Mr-Ojii ノーツを白くするために追加。
+            switch (si.nプレイヤー)
             {
                 case 0:
                     if (TJAPlayer3.Tx.Gauge_Soul_Explosion[0] != null)
-                        TJAPlayer3.Tx.Gauge_Soul_Explosion[0].t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[0], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[0], new Rectangle(st[i].ct進行.n現在の値 * TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Width, 0, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Width, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Height));
-                    TJAPlayer3.Tx.Notes.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[0], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[0], new Rectangle(st[i].Lane * 130, 0, 130, 130));
+                        TJAPlayer3.Tx.Gauge_Soul_Explosion[0].t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[0], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[0], new Rectangle(si.ct進行.n現在の値 * TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Width, 0, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Width, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Height));
+                    TJAPlayer3.Tx.Notes.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[0], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[0], new Rectangle(si.Lane * 130, 0, 130, 130));
                     if (TJAPlayer3.Tx.Notes_White != null)
-                        TJAPlayer3.Tx.Notes_White.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[0], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[0], new Rectangle(st[i].Lane * 130, 0, 130, 130));
+                        TJAPlayer3.Tx.Notes_White.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[0], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[0], new Rectangle(si.Lane * 130, 0, 130, 130));
                     break;
                 case 1:
                     if (TJAPlayer3.Tx.Gauge_Soul_Explosion[1] != null)
-                        TJAPlayer3.Tx.Gauge_Soul_Explosion[1].t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[1], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[1], new Rectangle(st[i].ct進行.n現在の値 * TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Width, 0, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Width, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Height));
-                    TJAPlayer3.Tx.Notes.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[1], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[1], new Rectangle(st[i].Lane * 130, 0, 130, 130));
+                        TJAPlayer3.Tx.Gauge_Soul_Explosion[1].t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[1], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[1], new Rectangle(si.ct進行.n現在の値 * TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Width, 0, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Width, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.NotesFlash.Height));
+                    TJAPlayer3.Tx.Notes.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[1], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[1], new Rectangle(si.Lane * 130, 0, 130, 130));
                     if (TJAPlayer3.Tx.Notes_White != null)
-                        TJAPlayer3.Tx.Notes_White.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[1], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[1], new Rectangle(st[i].Lane * 130, 0, 130, 130));
+                        TJAPlayer3.Tx.Notes_White.t2D拡大率考慮描画(TJAPlayer3.app.Device, CTexture.RefPnt.Center, TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointX[1], TJAPlayer3.app.Skin.SkinConfig.Game.Effect.FlyingNotes.EndPointY[1], new Rectangle(si.Lane * 130, 0, 130, 130));
                     break;
             }
             TJAPlayer3.Tx.Notes.Opacity = 255;//2020.05.15 Mr-Ojii これ書いとかないと、流れるノーツも半透明化されてしまう。
@@ -99,7 +100,7 @@ internal class CActChipEffects : CActivity
     private struct STチップエフェクト
     {
         public bool b使用中;
-        public CCounter ct進行;
+        public CCounter? ct進行;
         public int nプレイヤー;
         public int Lane;
     }
